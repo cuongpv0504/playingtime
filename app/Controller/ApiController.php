ef3fdc4ba0dd711b902a8d845ff6b33b33dc8a31
Pham Van Cuong <phamcuong@Phams-Mac-mini.local>
2019-01-28 14:21:22 +0700

    add home, auth, adding role

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -6,20 +6,85 @@ class ApiController extends AppController
 {
 	public $uses = array('User','Leave','Off');
 
+	//Auth
+	private function auth()
+	{
+		// $this->autoRender = false;
+		$email = $_SERVER['HTTP_USER_EMAIL'];
+		$check = $this->User->find('first',array(
+			'conditions' => array(
+				'email' => $email
+			)
+		));
+
+		if (empty($check)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public function login()
+	{
+		//to do
+	}
+
 	//return home data
 	public function home()
 	{
 		$this->autoRender = false;
+
+		if (!$this->auth()) {
+			return json_encode(array(
+				'error' => 'Can not authenicate'
+			));
+		}
+
 		$offData = $this->Off->find('all');
 		$leaveData = $this->Leave->find('all');
 
-		// pr($offData);
-		// pr($leaveData);
+		$data = array();
 
-		$data = array(
-			'off' => $offData,
-			'leave' => $leaveData
-		);
+		foreach ($offData as $key => $value) {
+			$data[] = array(
+				'id' => $value['Off']['id'],
+				'user_id' => $value['Off']['user_id'],
+				'duration' => $value['Off']['duration'],
+				'dates' => $value['Off']['dates'],
+				'create_at' => $value['Off']['create_at'],
+				'reason' => $value['Off']['reason'],
+				'emotion' => $value['Off']['emotion'],
+				'type' => $value['Type']['description'],
+				'day_left' => $value['Off']['day_left'],
+				'status' => $value['Status']['status'],
+				'time' => strtotime($value['Off']['create_at']),
+				'info' => 'off'
+			);
+		}
+
+		foreach ($leaveData as $key => $value) {
+			$data[] = array(
+				'id' => $value['Leave']['id'],
+				'user_id' => $value['Leave']['user_id'],
+				'start' => $value['Leave']['start'],
+				'end' => $value['Leave']['end'],
+				'date' => $value['Leave']['date'],
+				'create_at' => $value['Leave']['create_at'],
+				'reason' => $value['Leave']['reason'],
+				'emotion' => $value['Leave']['emotion'],
+				'status' => $value['Status']['status'],
+				'time' => strtotime($value['Leave']['create_at']),
+				'info' => 'leave'
+			);
+		}
+
+		function build_sorter($key) {
+		    return function ($a, $b) use ($key) {
+		        return $a[$key] - $b[$key];
+		    };
+		}
+
+		usort($data, build_sorter('time'));
 
 		return json_encode($data, JSON_PRETTY_PRINT);
 	}
@@ -34,9 +99,25 @@ class ApiController extends AppController
 	public function viewProfile()
 	{
 		$this->autoRender = false;
-		$userData = $this->User->find('all');
 
-		return json_encode($userData, JSON_PRETTY_PRINT);
+		if (!$this->auth()) {
+			return json_encode(array(
+				'error' => 'Can not authenicate'
+			));
+		}
+
+		$email = $_SERVER['HTTP_USER_EMAIL'];
+
+		$userData = $this->User->find(
+			'first',
+			array(
+				'conditions' => array(
+					'email' => $email
+				)
+			)
+		);
+
+		return json_encode($userData['User'], JSON_PRETTY_PRINT);
 	}
 
 	//view Leave Detail
@@ -63,6 +144,58 @@ class ApiController extends AppController
 		
 	}
 
-	//
+	public function test()
+	{
+		$this->autoRender = false;
+		$offData = $this->Off->find('all');
+		$leaveData = $this->Leave->find('all');
+
+		$data = array();
+
+		foreach ($offData as $key => $value) {
+			$data[] = array(
+				'id' => $value['Off']['id'],
+				'user_id' => $value['Off']['user_id'],
+				'duration' => $value['Off']['duration'],
+				'dates' => $value['Off']['dates'],
+				'create_at' => $value['Off']['create_at'],
+				'reason' => $value['Off']['reason'],
+				'emotion' => $value['Off']['emotion'],
+				'type' => $value['Type']['description'],
+				'day_left' => $value['Off']['day_left'],
+				'status' => $value['Status']['status'],
+				'time' => strtotime($value['Off']['create_at']),
+				'info' => 'off'
+			);
+		}
+
+		foreach ($leaveData as $key => $value) {
+			$data[] = array(
+				'id' => $value['Leave']['id'],
+				'user_id' => $value['Leave']['user_id'],
+				'start' => $value['Leave']['start'],
+				'end' => $value['Leave']['end'],
+				'date' => $value['Leave']['date'],
+				'create_at' => $value['Leave']['create_at'],
+				'reason' => $value['Leave']['reason'],
+				'emotion' => $value['Leave']['emotion'],
+				'status' => $value['Status']['status'],
+				'time' => strtotime($value['Leave']['create_at']),
+				'info' => 'leave'
+			);
+		}
+
+		function build_sorter($key) {
+		    return function ($a, $b) use ($key) {
+		        return $a[$key] - $b[$key];
+		    };
+		}
+
+		usort($data, build_sorter('time'));
+
+		pr($data);
+
+		// echo json_encode($data, JSON_PRETTY_PRINT);
+	}
 }
 ?>
--- app/Model/User.php
+++ app/Model/User.php
@@ -6,5 +6,11 @@ class User extends AppModel
 {
 	public $useTable = 't_users';
 	public $name = 'User';
+	public $belongsTo = array(
+        'Role' => array(
+            'className' => 'Role',
+            'foreignKey' => 'role'
+        )
+    );
 }
 ?>
